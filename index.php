545352166f30f0b9eaaca272cc12ef5c699e5a6d
Neidielli <100215137+Neidielli@users.noreply.github.com>
2023-01-17 23:31:04 -0300

    Autenticação do usuário

--- index.php
+++ index.php
@@ -6,10 +6,22 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
     <style>
+      .container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        width: 100%;
+        height: calc(100vh - 90px);
+      }
       .card-login {
         padding: 30px 0 0 0;
-        width: 350px;
-        margin: 0 auto;
+        width: 450px;
+        /* margin: 0 auto; */
+      }
+      .card-header {
+        display: flex;
+        align-items: center;
+        justify-content: center;
       }
     </style>
   </head>
@@ -31,17 +43,27 @@
             <div class="card-header">
               Login
             </div>
+
             <div class="card-body">
-              <form>
+              <form action="valida_login.php" method="post"><!-- ao clicar no submmit, envia as informações do forms -->
                 <div class="form-group">
-                  <input type="email" class="form-control" placeholder="E-mail">
+                  <input name="email" type="email" class="form-control" placeholder="E-mail">
                 </div>
                 <div class="form-group">
-                  <input type="password" class="form-control" placeholder="Senha">
+                  <input name="senha" type="password" class="form-control" placeholder="Senha">
                 </div>
+
+                <?php if(isset($_GET['login']) && $_GET['login'] == 'erro') { ?> 
+                    <div class="text-danger">
+                      Usuário ou senha inválido(s)
+                    </div>
+                <?php } ?> 
+                
+
                 <button class="btn btn-lg btn-info btn-block" type="submit">Entrar</button>
               </form>
             </div>
+
           </div>
         </div>
     </div>
---
+++ valida_login.php
@@ -0,0 +1,24 @@
+<?php
+
+    // var para verificar se a autenticação foi realizada
+    $usuario_autenticado = false;
+
+    // Lista de usuários válidos
+    $usuarios_app = array(
+        array('email' => 'adm@teste.com', 'senha' => '123'),
+        array('email' => 'user@teste.com', 'senha' => 'abc'),
+    );
+    // percorrer o array de usuarios para verificar se email passado é válido
+    foreach($usuarios_app as $user) { // o uso dos as nos dá acesso a cada um dos array de forma individual
+        
+        if($user['email'] == $_POST['email'] && $user['senha'] == $_POST['senha']) {
+            $usuario_autenticado = true;
+        }
+        
+    }
+
+    if($usuario_autenticado) {
+        echo 'Usuário autenticado';
+    } else {
+        header('Location: index.php?login=erro'); // header espera um destino
+    }
