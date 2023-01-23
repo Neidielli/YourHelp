dbe4653539f0a9e011d81c13c8b6ea2ae871d018
Neidielli <100215137+Neidielli@users.noreply.github.com>
2023-01-22 21:10:59 -0300

    registrando chamados

---
+++ registra_chamado.php
@@ -0,0 +1,17 @@
+<?php
+    // trabalhando na montagem do texto
+    $titulo = str_replace('#', '-', $_POST['titulo']);
+    $categoria = str_replace('#', '-', $_POST['categoria']);
+    $descricao = str_replace('#', '-', $_POST['descricao']);
+
+    $texto = $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+
+    // trabalhando na montagem do arquivo
+    $arquivo = fopen('arquivo.txt', 'a'); // abre o arquivo
+
+    fwrite($arquivo , $texto); // escreve no arquivo
+
+    fclose($arquivo); // fecha o arquivo
+
+    header('Location: abrir_chamado.php');
+?>
